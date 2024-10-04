5455262c4c50045c2f35649ea6637856c252d630
Ali Mahdavi <ivadham@users.noreply.github.com>
2024-10-05 00:45:26 +0330

    Add project files.

---
+++ RazorPagesMovie/Model/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Model
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseData { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+        public int Rating { get; set; }
+    }
+}
